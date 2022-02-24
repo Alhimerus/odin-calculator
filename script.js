16205e4beed0b0083ae6e06e35d5d8204c88a5a8
Alhimerus <Alhimerus@gmail.com>
2022-02-24 01:23:28 +0100

    Add operate function

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ function divide(a, b) {
   if (b === 0) return "No";
   return a / b;
 }
+
+function operate(number1, operator, number2) {
+  switch (operator) {
+    case "+":
+      return add(number1, number2);
+    case "-":
+      return subtract(number1, number2);
+    case "*":
+      return multiply(number1, number2);
+    case "/":
+      return divide(number1, number2);
+    default:
+      return "Wrong operator!";
+  }
+}
